74b53ca0e4b65242487fa9485b215a4655bb5741
Pablo Mecchia <pablomecchia90@hotmail.com>
2024-11-19 22:39:07 -0300

    Adding ping route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -31,11 +31,11 @@ func routes(_ *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/reservation-summary", handlers.Repo.ReservSum)
+	mux.Get("/ping", handlers.Repo.Ping)
 
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
-	mux.Get("/ping", handlers.Repo.Ping)
 
 	// Declaración que sirve para cargar todos los recursos estáticos del folder static
 	mux.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
